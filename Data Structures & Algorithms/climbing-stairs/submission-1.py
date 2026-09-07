1527f90c1555f03c400ac32204ebf5debe403824
Francesco Sacco <francescosacco.github@gmail.com>
2026-09-07 12:41:01 +0200

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        one_step_before: int = 2
+        two_steps_before: int = 1
+        for _ in range(3, n + 1):
+            current = one_step_before + two_steps_before
+            two_steps_before = one_step_before
+            one_step_before = current
+        return one_step_before
