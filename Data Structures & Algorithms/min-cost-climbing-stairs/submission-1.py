3d774fa0627c2e1ed40f00e06e74c2235a6f8e91
Francesco Sacco <francescosacco.github@gmail.com>
2026-09-07 12:41:37 +0200

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        prev2: int = 0
+        prev1: int = 0
+        for c in cost:
+            current: int = c + min(prev1, prev2)
+            prev2 = prev1
+            prev1 = current
+        return min(prev1, prev2)
